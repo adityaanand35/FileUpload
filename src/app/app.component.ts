54b5d88237e1c74af8c0d681317145989faa8713
Aditya <aditya.anand@habilelabs.io>
2019-04-13 12:05:56 +0530

    upload multiple files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,25 @@ import { HttpClient, HttpEventType } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  selectedFile: File = null;
+  selectedFile: FileList;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
   setFile(event) {
-    this.selectedFile = <File>event.target.files[0];
+    this.selectedFile = event.target.files;
+    // console.log(event.target.files);
+    // for (let i = 0; i < event.target.files.length; i++) {
+    //   this.selectedFile.item[i] = <File>event.target.files[i];
+    // }
   }
 
   uploadFile() {
     const fd = new FormData();
-    fd.append('image', this.selectedFile, this.selectedFile.name);
+    fd.append('contractorId', "123");
+    for (let i = 0; i < this.selectedFile.length; i++) {
+      console.log(this.selectedFile.item(i));
+      fd.append('image', this.selectedFile.item(i), this.selectedFile.item.name);
+    }
+    console.log("fd",fd);
     this.http.post('<url>', fd, {
       reportProgress: true,
       observe: 'events'
@@ -27,7 +36,7 @@ export class AppComponent {
         console.log(event);
       }
 
-      
+
     })
   }
 }
