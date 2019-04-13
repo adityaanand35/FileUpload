5ddf59ee5e15ef7550a9528332419025760ef97f
Aditya <aditya.anand@habilelabs.io>
2019-04-13 11:34:17 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpEventType } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FileUploadApp';
+  selectedFile: File = null;
+
+  constructor(private http: HttpClient) {}
+  setFile(event) {
+    this.selectedFile = <File>event.target.files[0];
+  }
+
+  uploadFile() {
+    const fd = new FormData();
+    fd.append('image', this.selectedFile, this.selectedFile.name);
+    this.http.post('<url>', fd, {
+      reportProgress: true,
+      observe: 'events'
+    }).subscribe(event => {
+      if (event.type === HttpEventType.UploadProgress) {
+        console.log('Upload Progress: ', Math.round(event.loaded / event.total * 100), '%');
+      } else if (event.type === HttpEventType.Response) {
+        console.log(event);
+      }
+
+      
+    })
+  }
 }
